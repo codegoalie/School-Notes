191d875d88db12349815982236dedcacc0b1139c
Chris Marshall <chris@chrismar035.com>
2010-11-15 18:40:02 -0500

    moved database notes into databases/, added untested methoed to mysql-cell

--- databases/mysql-cellophane/mysql-cellophane.rb
+++ databases/mysql-cellophane/mysql-cellophane.rb
@@ -63,6 +63,31 @@ class MySQLc
     @query << "AND #{@extra_cond}" unless @extra_cond.nil?
   end
 
+
+  # private method set_attributes
+  #
+  # sets instance attributes from fucntion parameters
+  # called by each public API methods
+  def set_attributes(*vals)
+    @table      = vals[0] unless vals.size < 1
+    @field_list = vals[1] unless vals.size < 2
+    @id_field   = vals[2] unless vals.size < 3
+    @id         = vals[3] unless vals.size < 4
+    @extra_cond = vals[4] unless vals.size < 5
+  end
+
+  # public method quoted_string_from
+  #
+  # library function to convert values
+  # in a hash (or plain array) into an
+  # escaped single quoted comma
+  # delimited string
+  def quoted_string_from hash_or_array
+    array =  hash_or_array.class == Hash ? hash_or_array.values : hash_or_array
+    array.collect { |raw| "'#{esc(raw)}'"}.join(',')
+  end
+
+
   # public method esc
   #
   # takes string parameter
@@ -72,6 +97,6 @@ class MySQLc
   end
 
 
-  #private :connect
+  #private :connect, :set_attributes
 end
 
