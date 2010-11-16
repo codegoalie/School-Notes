770ca62f755052a5388ccaaa99e800852bce9bd4
Chris Marshall <chris@chrismar035.com>
2010-11-15 22:29:39 -0500

    Finished bankers algo

--- OS/ass4/banker.rb
+++ OS/ass4/banker.rb
@@ -1,24 +1,70 @@
 RESOURCE_COUNT = 5
 INITIAL_PROCESSES = 4
-MAX_RESOURCES = 10
+MAX_RESOURCES = 5
 
-available_resources = Array.new(RESOURCE_COUNT)
+def banker(available_resources, allocated, max)
+  if(allocated.size == 0)
+    return true
+  else
+    orig_size = allocated.size
+    #print "#{orig_size} processes to check\n"
+    checking_process = 0
+    while(checking_process < allocated.size)
+      if(resources_available(available_resources, allocated[checking_process], max[checking_process]))
+        #release_resources
+        RESOURCE_COUNT.times do |i|
+          available_resources[i] += allocated[checking_process][i]
+        end
+        allocated.delete_at checking_process
+        max.delete_at checking_process
+        #print "process #{checking_process} finishes\n"
+      else
+        checking_process += 1
+      end
+    end
+    if(orig_size == allocated.size)
+      return false
+    else
+      return banker(available_resources, allocated, max)
+    end
+  end
+end
+
+def resources_available(available_resources, allocated, max)
+  RESOURCE_COUNT.times do |i|
+    #print "max #{max[i]}\n"
+    #print "alloc #{allocated[i]}\n"
+    #print "avail #{available_resources[i]}\n"
+    if(max[i]-allocated[i] > available_resources[i])
+      return false
+    end
+  end
+  return true
+end
+
+
+
+
 allocated = Array.new
 max = Array.new
 INITIAL_PROCESSES.times do
   process_allocated = Array.new
   process_max = Array.new
   RESOURCE_COUNT.times do
-    process_allocated << rand(MAX_RESOURCES)
-    process_max << rand(MAX_RESOURCES)
+    allocate = rand(MAX_RESOURCES)
+    process_allocated << allocate
+    begin
+      temp_max = rand(MAX_RESOURCES)
+    end while temp_max < allocate
+    process_max << temp_max
   end
   allocated << process_allocated
   max << process_max
 end
 
-state = Array.new()
+state = Array.new #(RESOURCE_COUNT, 0)
 RESOURCE_COUNT.times do 
-  state << rand(MAX_RESOURCES)
+  state << rand(MAX_RESOURCES/2)
 end
 
 print "Current State of Available Resources:\n"
@@ -32,27 +78,6 @@ INITIAL_PROCESSES.times do |i|
   print "P#{i} #{max[i].to_s}\n"
 end
 
-def banker(available_resources, allocated, max)
-  if(allocated.size == 0)
-    return true
-  else
-    checking_process = 0
-    while(checking_process < INITIAL_PROCESSES.size && safe_state = false) do
-      if(resources_available(available_resources, allocated[checking_process], max[checking_process]))
-        #release_resources
-        # this shoudl recurse at some point
-      end
-    end
-  end
-end
-
-def resources_available(available_resources, allocated, max)
-  RESOURCE_COUNT.times do |i|
-    if(max[0]-allocated[0] > available_resources)
-      return false
-    end
-  end
-  return true
-end
-
-
+print "\nThis is a "
+print banker(state, allocated, max) ? "SAFE" : "UNSAFE"
+print " state\n"
