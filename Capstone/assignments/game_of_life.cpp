939518a2e4d4e24af5021c429ef2cc68065eb159
Chris Marshall <chris@chrismar035.com>
2011-08-30 18:41:46 -0400

    Started Conway's Game of Life for Capstone

---
+++ Capstone/assignments/game_of_life.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int size;
+  vector< vector<int> > field;
+  cout << "What size grid would you like?";
+  cin >> size;
+
+
+
+  return 0;
+}
