ebd0d22285b5aae401baf582a9ec69f274dcee31
Chris Marshall <chris@chrismar035.com>
2011-09-26 07:14:07 -0400

    Finshed Stacking Boxes program

--- SpeedProgramming/stacking_boxes/p3.cpp
+++ SpeedProgramming/stacking_boxes/p3.cpp
@@ -1,122 +1,78 @@
 
 #include <iostream>
-#include <vector>
+#include <list>
+#include <list>
 #include <string>
+#include <algorithm>
 
 using namespace std;
+
+bool sort_desc(int a, int b)
+{
+  return a > b;
+};
 
 class Box
 {
   public:
-    vector<int> v;
+    list<int> v;
     int id;
 
-    Box(vector<int> p_v) : v(p_v) {};
+    Box(list<int> p_v) : v(p_v) {};
     Box(int p_id) : id(p_id) {};
 
     int size() { return int(v.size()); };
-    int operator[](int i) { return v[i]; };
 
     void push_back(int dim) { v.push_back(dim); };
 
-    void print() const
+    void print() const 
     {
-      for(int i = 0; i < int(v.size()); ++i)
+      for(list<int>::const_iterator i = v.begin(); i != v.end(); ++i)
       {
-        cout << " " <<  v[i];
+        cout << " " <<  *i;
       }
     };
 
-    bool operator<(const Box &rhs)
+    bool operator<(const Box &rhs) { return v < rhs.v; }
+    bool operator>(const Box &rhs) { return v > rhs.v; }
+
+    bool fits_inside_of(const Box &rhs)
     {
-      cout << "op: ";
-      print();
-      cout << " < ";
-      rhs.print();
-      cout << " == ";
-      for(int i = 0; i < int(v.size()); ++i)
+      list<int>::const_iterator r = rhs.v.begin();
+      for(list<int>::const_iterator i = v.begin(); i != v.end(); ++i)
       {
-        if(rhs.v[i] != v[i]) 
+        if((*r) <= (*i)) 
         {
-          cout << (rhs.v[i] < v[i]) << '\n';
-          return rhs.v[i] < v[i];
+          //cout << "\nFISO failed on " << (*r) << " < " << (*i) << endl;
+          return false;
         }
+        ++r;
       }
-      cout << "false\n";
-      return false;
-    }
+      return true;
+      
+    };
+
     
-    void sort() { sort(0); };
-    void sort(int start)
-    {
-      //cout << "  Starting sort" << endl;
-      if(start != int(v.size())-1)
-      {
-        int max = v[start], max_i= start;
-        for(int i = start+1; i < int(v.size()); ++i)
-        {
-          //cout << "  " << v[i] << " < " << max << endl;
-          if(v[i] > max)
-          {
-            max = v[i];
-            max_i = i;
-          }
-        }
-        //cout << " " << max_i << " " << start << endl;
-        if(start != max_i)
-        {
-          //cout << "  Swapping " << start << " and " << max_i << endl;
-          v[max_i] = v[start];
-          v[start] = max;
-        }
-        sort(start+1);
-      }
-    };
+    void sort() { v.sort(sort_desc); };
 };
 
-void sort(int start, vector<Box> &v)
-{
-  //cout << "  Starting sort" << endl;
-  if(start != int(v.size())-1)
-  {
-    Box max = v[start];
-    int max_i = start;
-    for(int i = start+1; i < int(v.size()); ++i)
-    {
-      //cout << "  " << v[i] << " < " << max << endl;
-      if(v[i] < max)
-      {
-        max = v[i];
-        max_i = i;
-      }
-    }
-    //cout << " " << max_i << " " << start << endl;
-    if(start != max_i)
-    {
-      //cout << "  Swapping " << start << " and " << max_i << endl;
-      v[max_i] = v[start];
-      v[start] = max;
-    }
-    sort(start+1, v);
-  }
-};
+//list<int> streak_builder(Box current_box, list<Box> smaller_boxes)
+//{
+//  list<int> best_streak;
+//  for(int i = 0; i < int(smaller_boxes.size()); ++i)
+//  {
+//    if(smaller_boxes[i] < current_box)
+//    {
+//      list<Box> new_smaller = smaller_boxes;
+//      new_smaller.erase(smaller_boxes.begin());
+//      list<int> new_streak = streak_builder(smaller_boxes[i], new_smaller);
+//      if(int(best_streak.size()) < int(new_streak.size()))
+//        best_streak = new_streak;
+//    }
+//  }
+//  return best_streak;
+//};
 
-vector<int> streak_builder(Box current_box, vector<Box> smaller_boxes)
-{
-  vector<int> best_streak;
-  for(int i = 0; i < int(smaller_boxes.size()); ++i)
-  {
-    if(smaller_boxes[i] < current_box)
-    {
-      vector<Box> new_smaller = smaller_boxes;
-      new_smaller.erase(smaller_boxes.begin());
-      vector<int> new_streak = streak_builder(smaller_boxes[i], new_smaller);
-      if(int(best_streak.size()) < int(new_streak.size()))
-        best_streak = new_streak;
-    }
-  }
-  return best_streak;
-};
 
 int main()
 {
@@ -128,7 +84,7 @@ int main()
   while(!cin.eof())
   {
 
-    vector<Box> boxes;
+    list<Box> boxes;
     for(int i = 0; i < box_count; ++i)
     {
       Box box(i+1);
@@ -141,41 +97,53 @@ int main()
       boxes.push_back(box);
     }
 
-    for(int i = 0; i < int(boxes.size()); ++i)
+    for(list<Box>::iterator i = boxes.begin(); i != boxes.end(); ++i)
     {
-      boxes[i].sort();
+      i->sort();
     }
 
-    sort(0, boxes);
+    boxes.sort();
 
 
-    vector<int> streak, best_streak;
-    vector<Box>::iterator it = boxes.begin();
-    for(int i = 0; i < int(boxes.size()); ++i)
+ // cout << "Read all boxes:\n";
+ // for(list<Box>::iterator i = boxes.begin(); i != boxes.end(); ++i)
+ // {
+ //   cout << "Box " << i->id << endl << " ";
+ //   for(list<int>::iterator ii = i->v.begin(); ii != i->v.end(); ++ii)
+ //   {
+ //     cout << " " << *ii;
+ //   }
+ //   cout << endl << endl;
+ // }
+
+    list<Box> streak;
+    do
     {
-      vector<Box> smaller_boxes = boxes;
-      smaller_boxes.erase(it);
-      streak = streak_builder(boxes[i], smaller_boxes);
-      if(int(best_streak.size()) < int(streak.size()))
-        best_streak = streak;
-      ++it;
+      //cout << "\nStreaking at " << i->id << ", ";
+      list<Box> current_streak;
+      current_streak.push_back(boxes.front());
+      list<Box>::iterator ii = boxes.begin();
+      ++ii;
+      for(; ii != boxes.end(); ++ii)
+      {
+        //cout << " %" << current_streak.back().id << ":" << ii->id << "% ";
+        if((current_streak.back()).fits_inside_of(*ii))
+        {
+          //cout << ii->id << ", ";
+          current_streak.push_back(*ii);
+        }
+      }
+      if(current_streak.size() > streak.size())
+        streak = current_streak;
+    }while ( next_permutation(boxes.begin(), boxes.end()));
+
+    cout << streak.size() << endl;
+    for(list<Box>::iterator i = streak.begin(); i != streak.end(); ++i)
+    {
+      cout << i->id << " ";
     }
-    cout << "Best: " << best_streak.size() << " -- ";
-    for(int i = 0; i < int(best_streak.size()); ++i)
-      cout << " " << best_streak[i];
     cout << endl;
 
-    cout << "Read all boxes:\n";
-    for(int i = 0; i < int(boxes.size()); ++i)
-    {
-      cout << "Box " << boxes[i].id << endl << " ";
-      boxes[i].sort();
-      for(int ii = 0; ii < boxes[i].size(); ++ii)
-      {
-        cout << " " << boxes[i][ii];
-      }
-      cout << endl << endl;
-    }
 
     cin >> box_count;
     cin >> dims;
